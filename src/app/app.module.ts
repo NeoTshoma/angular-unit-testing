d787afd6d05fb244d77974afa3e8388b38d1e2f8
Neo Tshoma <neo.tshoma@tangentsolutions.co.za>
2023-03-24 14:56:01 +0200

    Created the navigation and added the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ResultsViewComponent } from './components/results-view/results-view.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ResultsViewComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
