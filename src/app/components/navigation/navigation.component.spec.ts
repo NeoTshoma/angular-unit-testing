458b6729bba23be7c4f690f4f0b377df23f9328e
Neo Tshoma <neo.tshoma@tangentsolutions.co.za>
2023-05-25 21:23:14 +0200

    Writing unit tests

--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -1,17 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { NavigationComponent } from './navigation.component';
+import { ITunesState, iTunesInitialState } from 'src/app/store/state/itunes.state';
+import { Store } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let mockStore: MockStore<{
+    iTunesSearchResults: [];
+    itunesSearchError: false;
+    itunesSearchErrors: null;
+    itunesSearchLoading: false;
+  }>;
+  let initialState: ITunesState = {
+    ...iTunesInitialState
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
-    })
-    .compileComponents();
+      declarations: [NavigationComponent],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+      ],
+      providers: [
+        provideMockStore({
+          initialState
+        })
+      ]
+    }).compileComponents();
 
+    mockStore = TestBed.get(Store);
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +42,13 @@ describe('NavigationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should dispatch an action to search', () => {
+    spyOn(mockStore, 'dispatch');
+    component.searchTerm = 'Sound 1';
+
+    component.searchItunes();
+
+    expect(mockStore.dispatch).toHaveBeenCalled();
+  })
 });
--- src/app/components/results-view/results-view.component.spec.ts
+++ src/app/components/results-view/results-view.component.spec.ts
@@ -1,17 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultsViewComponent } from './results-view.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import {
+  ITunesState,
+  iTunesInitialState,
+} from 'src/app/store/state/itunes.state';
+import { Store } from '@ngrx/store';
+import { AngMusicPlayerComponent } from 'ang-music-player';
+import { getItunesSearchError, getItunesSearchLoading, getItunesSearchResultSet } from 'src/app/store/selectors/itunes.selector';
 
 describe('ResultsViewComponent', () => {
   let component: ResultsViewComponent;
   let fixture: ComponentFixture<ResultsViewComponent>;
+  let mockStore: MockStore<{
+    iTunesSearchResults: [];
+    itunesSearchError: false;
+    itunesSearchErrors: null;
+    itunesSearchLoading: false;
+  }>;
+  let initialState: ITunesState = {
+    ...iTunesInitialState,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResultsViewComponent ]
-    })
-    .compileComponents();
+      declarations: [ResultsViewComponent, AngMusicPlayerComponent],
+      providers: [
+        provideMockStore({
+          initialState,
+        }),
+      ],
+    }).compileComponents();
 
+    mockStore = TestBed.get(Store);
     fixture = TestBed.createComponent(ResultsViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +41,18 @@ describe('ResultsViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set add the result set to the audio list on Init', () => {
+    mockStore.overrideSelector(getItunesSearchResultSet, [
+      { artistId: 1, artistName: 'Test 1 Artist' },
+      { artistId: 2, artistName: 'Test 2 Artist' },
+    ]);
+    mockStore.overrideSelector(getItunesSearchLoading, false);
+    mockStore.overrideSelector(getItunesSearchError, false);
+
+    component.ngOnInit();
+
+    expect(component.audioList.length).toEqual(2);
   });
 });
